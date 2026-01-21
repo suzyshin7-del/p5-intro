abf34a138aa5c15a59c9d88b083c098f066d6d82
suzyshin7-del <suzy.shin7@gmail.com>
2026-01-21 19:59:32 +0000

    Exersice 2 Interim

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-/* CODE GOES HERE */
+let list = [];
+
+
+function preload(){
+    list = loadJSON("data.json");
+    
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textAlign("center");
+  textSize(24);
+  text("Canada's Exports", 200, 40);
+
+  Object.values(list).map((item, index) => {
+    rect(50, index * 30 + 70, item.amount, 10);
+    textSize(12);
+    textAlign("left");
+    text(item.name, 50, index * 30 + 65);
+  });
+
+}
+
